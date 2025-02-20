34024a3abb31061a2a59178d73320b5a316b62be
nickperi <137534291+nickperi@users.noreply.github.com>
2025-02-20 17:38:04 +0000

    settings.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from polls import views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
