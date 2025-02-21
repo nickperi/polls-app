cfb22f5fe8349ba790947188f7803b99329dd8bc
nickperi <nicholaspariag27@gmail.com>
2025-02-20 23:56:27 -0400

    tests.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -20,10 +20,6 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
