0ce7f98b48043b089011e9d29bfa6b844e4327e9
nickperi <137534291+nickperi@users.noreply.github.com>
2025-07-20 05:25:54 -0400

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -19,18 +19,11 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
 
     def get_queryset(self):
-<<<<<<< HEAD
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
-=======
         """
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
-            :5
-        ]
-
->>>>>>> 51741f38ba52c51b1564f50db773263cafab41ef
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
