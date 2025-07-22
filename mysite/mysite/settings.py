3998d77d02da3a51a217afed52962912337e1622
nickperi <nicholaspariag27@gmail.com>
2025-07-22 13:29:36 -0400

    updated requirements

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,6 +15,8 @@ import dj_database_url
 from dotenv import load_dotenv
 from pathlib import Path
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,12 +26,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = "django-insecure-@c1a!22kt-$+emb79^9-(!-eyfy96vhok4phxo#aw(6*wux(#8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+#DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
+#ALLOWED_HOSTS = ['polls-app-9k92.onrender.com', '127.0.0.1']
+#CSRF_TRUSTED_ORIGINS = ['https://polls-app-9k92.onrender.com']
 
 
 # Application definition
@@ -46,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -81,8 +88,9 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    
 }
+
+#DATABASE_URL = os.environ.get('DATABASE_URL')
 
 
 # Password validation
